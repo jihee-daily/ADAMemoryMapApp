75703335ac331fd1baeac0265f4d67ce6c1d5d4d
jihee-daily <kim76955@gmail.com>
2024-03-13 15:29:12 +0900

    feat: add list as a custom animated menu

--- ADAMemoryMapApp/ADAMemoryMapApp/Models/Location.swift
+++ ADAMemoryMapApp/ADAMemoryMapApp/Models/Location.swift
@@ -8,7 +8,8 @@
 import Foundation
 import MapKit
 
-struct Location: Identifiable {
+struct Location: Identifiable, Equatable {
+    
     let name: String
     let cityName: String
     let coordinates: CLLocationCoordinate2D
@@ -22,5 +23,10 @@ struct Location: Identifiable {
         // cityName = "Rome"
         // id = "ColosseumRome"
         name + cityName
+    }
+    
+    // Equatable
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
     }
 }
